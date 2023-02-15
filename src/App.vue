1b9cb745480f1afdc00f8abe330fb3e31772f023
zhitenev.andr <zhitenev.andr@gmail.com>
2023-02-16 01:06:43 +0300

    wrap title & paragraph, added copy html

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
-import defaultImg from "@/assets/img/image.png";
 import { ref, onMounted } from "vue";
 
 const visualView = ref(null);
 const historyUndo = ref([]);
 const historyRedo = ref([]);
+const selectedTextLength = ref(0);
 let clickedUndo = false;
+let correctPos = 0;
 
 function highlight() {
   document.querySelectorAll(".visual-view > *").forEach((el) => {
@@ -15,15 +16,19 @@ function highlight() {
   });
   if (this.nodeName === "IMG") {
     this.classList.add("active-el");
-    if (window.getSelection) {
-      if (window.getSelection().empty) {
-        window.getSelection().empty();
-      } else if (window.getSelection().removeAllRanges) {
-        window.getSelection().removeAllRanges();
-      }
-    } else if (document.selection) {
-      document.selection.empty();
+    removeSelection();
+  }
+}
+
+function removeSelection() {
+  if (window.getSelection) {
+    if (window.getSelection().empty) {
+      window.getSelection().empty();
+    } else if (window.getSelection().removeAllRanges) {
+      window.getSelection().removeAllRanges();
     }
+  } else if (document.selection) {
+    document.selection.empty();
   }
 }
 
@@ -78,12 +83,75 @@ function redo() {
   historyRedo.value.pop();
 }
 
-function makeTitle() {
-  console.log("title");
-}
+function wrap(tag) {
+  const selection = window.getSelection().getRangeAt(0);
+  const node = window.getSelection().getRangeAt(0).startContainer.parentNode;
+  const nodeText = node.innerText;
+  const selectedText = selection.extractContents();
 
-function makeParagraph() {
-  console.log("paragraph");
+  if (selectedText.querySelector("img")) {
+    selectedTextLength.value = 0;
+    selection.insertNode(selectedText);
+    removeSelection();
+
+    const tagName = node.nodeName.toLowerCase();
+    const slicedIndex = Array.from(node.parentNode.children).indexOf(node);
+    const prev = node.parentNode.children[slicedIndex];
+    const next = node.parentNode.children[slicedIndex + 1];
+    if (prev.nodeName === next.nodeName) {
+      const restoredElem = document.createElement(tagName);
+      restoredElem.innerText = nodeText;
+      prev.remove();
+      next.remove();
+      document
+        .querySelector(".visual-view")
+        .insertBefore(
+          restoredElem,
+          document.querySelector(".visual-view").children[slicedIndex]
+        );
+    }
+
+    alert("Выделите текст без картинки");
+    return;
+  }
+
+  const afterText = node.innerText;
+  let wrapElement;
+
+  if (tag === "title") {
+    wrapElement = document.createElement("h3");
+  }
+  if (tag === "paragraph") {
+    wrapElement = document.createElement("p");
+  }
+
+  wrapElement.appendChild(selectedText);
+  const beforeText = node.innerText;
+  const newTag = node.nodeName.toLowerCase();
+  const beforeElement = document.createElement(newTag);
+  beforeElement.innerText = beforeText.slice(
+    0,
+    window.getSelection().anchorOffset - 1
+  );
+  const afterElement = document.createElement(newTag);
+  afterElement.innerText = afterText.slice(
+    window.getSelection().anchorOffset - 1 + wrapElement.innerText.length
+  );
+
+  const nodeIndex = Array.from(node.parentNode.children).indexOf(node);
+  node.parentNode.insertBefore(
+    afterElement,
+    node.parentNode.children[nodeIndex]
+  );
+  node.parentNode.insertBefore(
+    wrapElement,
+    node.parentNode.children[nodeIndex]
+  );
+  node.parentNode.insertBefore(
+    beforeElement,
+    node.parentNode.children[nodeIndex]
+  );
+  node.remove();
 }
 
 function placeImage() {
@@ -91,28 +159,44 @@ function placeImage() {
 }
 
 function copyHtml() {
-  console.log("html");
+  navigator.clipboard.write([
+    new ClipboardItem({
+      "text/html": new Blob([document.querySelector(".visual-view").innerHTML], { type: "text/html" }),
+    }),
+  ]);
+  alert("Сохранено в виде HTML");
+  const pos = document.querySelector(".container").getBoundingClientRect().x;
+  if (pos !== correctPos) {
+    let move = correctPos - pos;
+    document.querySelector(".container").style.transform = `translate(-${move}px, -${move}px)`;
+  }
 }
 
 function keyDownHandler(e) {
-  if ((e.key.length === 1 || e.key === 'Delete' || e.key === 'Enter' || e.key === 'Backspace') && !e.ctrlKey) {
+  if (
+    (e.key.length === 1 ||
+      e.key === "Delete" ||
+      e.key === "Enter" ||
+      e.key === "Backspace") &&
+    !e.ctrlKey
+  ) {
     const elem = visualView.value.cloneNode(true);
     historyUndo.value.push(elem);
     historyRedo.value = [];
     clickedUndo = false;
   }
 
-  if (e.key === 'Delete') {
+  if (e.key === "Delete") {
     historyUndo.value.pop();
-    const selected = document.querySelector('.active-el');
-    selected.classList.remove('active-el');
+    const selected = document.querySelector(".active-el");
+    selected.classList.remove("active-el");
     const elem = visualView.value.cloneNode(true);
     historyUndo.value.push(elem);
     selected.remove();
   }
 
   const key = e.key;
-  if (key.toString() === 'z' && e.ctrlKey && historyUndo.value.length > 0) {
+  if (key.toString() === "z" && e.ctrlKey && historyUndo.value.length > 0) {
     undo();
   }
 }
@@ -121,7 +205,7 @@ onMounted(() => {
   document.querySelectorAll(".visual-view > *").forEach((el) => {
     el.addEventListener("click", highlight);
   });
-  document.addEventListener('click', (e) => {
+  document.addEventListener("click", (e) => {
     if (e.target.nodeName !== "IMG") {
       document.querySelectorAll(".visual-view > *").forEach((el) => {
         if (el.classList.value.includes("active")) {
@@ -130,9 +214,20 @@ onMounted(() => {
       });
     }
   });
+  document.querySelector(".visual-view").addEventListener("mouseup", () => {
+    try {
+      selectedTextLength.value = window
+      .getSelection()
+      .getRangeAt(0)
+      .toString().length;
+    } catch (error) {
+      console.log();
+    }
+  });
   document
     .querySelector(".visual-view")
     .addEventListener("keydown", keyDownHandler);
+  correctPos = document.querySelector(".container").getBoundingClientRect().x;
 });
 </script>
 
@@ -161,14 +256,16 @@ onMounted(() => {
           color="shadow"
           icon-color="#262824"
           class="mr-3"
-          @click="makeTitle"
+          :disabled="selectedTextLength === 0"
+          @click="wrap('title')"
         />
         <va-button
           icon="text_fields"
           color="shadow"
           icon-color="#262824"
           class="mr-3"
-          @click="makeParagraph"
+          :disabled="selectedTextLength === 0"
+          @click="wrap('paragraph')"
         />
         <va-button
           icon="image"
@@ -177,11 +274,12 @@ onMounted(() => {
           class="mr-3"
           @click="placeImage"
         />
-        <a href="#" class="va-link ml-3" @click="copyHtml">Скопировать HTML</a>
+        <a href="#" class="va-link ml-3" @click="copyHtml"
+          >Скопировать HTML</a
+        >
       </div>
     </div>
     <div class="content">
-      <div>{{ history }}</div>
       <div class="visual-view" ref="visualView" contenteditable>
         <p>
           Таким образом консультация с широким активом представляет собой
@@ -198,12 +296,11 @@ onMounted(() => {
           нашей деятельности играет важную роль в формировании существенных
           финансовых и административных условий.
         </p>
-        <br />
-        <h3 class="va-h3">Смотрите какие обезьянки</h3>
-        <br />
-        <img :src="defaultImg" />
-        <br />
-        <br />
+        <h3>Смотрите какие обезьянки</h3>
+        <img
+          src="https://i.ibb.co/6Rz6d5D/image-1.png"
+          alt="Обезьянки"
+        />
         <p>
           Таким образом консультация с широким активом представляет собой
           интересный эксперимент проверки позиций, занимаемых участниками в
@@ -219,7 +316,6 @@ onMounted(() => {
           нашей деятельности играет важную роль в формировании существенных
           финансовых и административных условий.
         </p>
-        <br />
         <p>
           Товарищи! новая модель организационной деятельности требуют от нас
           анализа направлений прогрессивного развития. Задача организации, в
@@ -235,6 +331,10 @@ onMounted(() => {
 </template>
 
 <style lang="scss">
+* {
+  transition-property: none !important;
+}
+
 body {
   margin: 0;
   padding: 0;
@@ -242,6 +342,55 @@ body {
   scrollbar-width: thin;
   width: 100vw;
   overflow-x: hidden;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p {
+  color: currentColor;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  font-weight: 700 !important;
+}
+
+h1 {
+  font-size: 3rem !important;
+  line-height: 3.5rem !important;
+}
+
+h2 {
+  font-size: 2.5rem !important;
+  line-height: 3rem !important;
+}
+
+h3 {
+  font-size: 2rem !important;
+  line-height: 2.5rem !important;
+}
+
+h4 {
+  font-size: 1.75rem !important;
+  line-height: 2rem !important;
+}
+
+h5 {
+  font-size: 1.5rem !important;
+  line-height: 1.75rem !important;
+}
+
+h6 {
+  font-size: 1.25rem !important;
+  line-height: 1.5rem !important;
 }
 
 #app {
@@ -261,6 +410,7 @@ body {
   padding: 15px 30px;
   background: #fff;
   margin-bottom: 15px;
+  border-radius: 4px;
 }
 
 .content {
@@ -268,10 +418,19 @@ body {
   background: #fff;
   min-height: calc(100vh - 175px);
   margin-bottom: 30px;
+  border-radius: 4px;
 }
 
 .visual-view {
   min-height: calc(100vh - 205px);
+
+  & > * {
+    margin-bottom: 30px;
+
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
 }
 
 p {
@@ -282,6 +441,7 @@ p {
 
 img {
   max-height: 300px;
+  border-radius: 4px;
 }
 
 .active-el {
